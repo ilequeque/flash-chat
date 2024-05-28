57f234c5846aca5dea7f5cd5272dcc3a9d6da7b5
ilequeque <ilyas.shakhabdinov@gmail.com>
2024-05-28 12:13:06 +0500

    flash-chat app

---
+++ Flash Chat iOS13/Message.swift
@@ -0,0 +1,14 @@
+//
+//  Message.swift
+//  Flash Chat iOS13
+//
+//  Created by Iliyas Shakhabdin on 14.03.2024.
+//  Copyright © 2024 Angela Yu. All rights reserved.
+//
+
+import Foundation
+
+struct Message {
+    let sender: String
+    let body: String
+}
